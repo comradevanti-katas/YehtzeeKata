2cab152c16f6181317470f7a2c5065ab6d25596f
Ramon Brullo <kingvanti650@gmail.com>
2023-02-27 13:04:38 +0100

    feat: first few requirements
    Forgot to commit rip

---
+++ Yetzee/Yetzee.Tests/YetzeeTests.cs
@@ -0,0 +1,117 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace Yetzee;
+
+public class YetzeeTests
+{
+
+    private static int[] ParseDie(string s)
+    {
+        return s.Split(",").Select(int.Parse).ToArray();
+    }
+    
+    [Theory]
+    [InlineData("1,1,3,3,6", 14)]
+    [InlineData("4,5,5,6,1", 21)]
+    public void Chance_Is_Sum_Of_Die(string dieString, int expected)
+    {
+        var die = ParseDie(dieString);
+        var actual = YetzeeGame.Score(die, Category.Chance);
+        Assert.Equal(expected, actual);
+    }
+    
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(4)]
+    [InlineData(5)]
+    [InlineData(6)]
+    public void Yahtzee_Scores_50_If_All_Die_Have_Same_Value(int dieValue)
+    {
+        var die = Enumerable.Repeat(dieValue, 5).ToArray();
+        var actual = YetzeeGame.Score(die, Category.Yahtzee);
+        Assert.Equal(50, actual);
+    }
+
+    [Theory]
+    [InlineData("1,1,1,2,1")]
+    [InlineData("1,2,3,4,5")]
+    [InlineData("3,3,3,3,4")]
+    public void Yahtzee_Scores_0_If_Not_All_Die_Have_Same_Value(string dieString)
+    {
+        var die = ParseDie(dieString);
+        var actual = YetzeeGame.Score(die, Category.Yahtzee);
+        Assert.Equal(0, actual);
+    }
+
+    [Theory]
+    [InlineData("1,1,2,4,4", Category.Ones, 2)]
+    [InlineData("2,3,2,5,1", Category.Ones, 1)]
+    [InlineData("3,3,3,4,5", Category.Ones, 0)]
+    [InlineData("1,1,2,4,4", Category.Twos, 2)]
+    [InlineData("2,3,2,5,1", Category.Twos, 4)]
+    [InlineData("3,3,3,4,5", Category.Twos, 0)]
+    [InlineData("1,1,2,4,4", Category.Threes, 0)]
+    [InlineData("2,3,2,5,1", Category.Threes, 3)]
+    [InlineData("3,3,3,4,5", Category.Threes, 9)]
+    [InlineData("1,1,2,4,4", Category.Fours, 8)]
+    [InlineData("2,3,2,5,1", Category.Fours, 0)]
+    [InlineData("3,3,3,4,5", Category.Fours, 4)]
+    [InlineData("1,1,2,4,4", Category.Fives, 0)]
+    [InlineData("2,3,2,5,1", Category.Fives, 5)]
+    [InlineData("3,5,3,4,5", Category.Fives, 10)]
+    [InlineData("1,1,2,4,4", Category.Sixes, 0)]
+    [InlineData("6,3,2,5,1", Category.Sixes, 6)]
+    [InlineData("3,5,6,6,6", Category.Sixes, 18)]
+    public void Numbers_Score_The_Sum_Of_The_Die_With_The_Number(string dieString, Category category, int expected)
+    {
+        var die = ParseDie(dieString);
+        var actual = YetzeeGame.Score(die, category);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("2,2,1,3,4", 4)]
+    [InlineData("1,2,4,3,3", 6)]
+    public void A_Pair_Is_Scored_As_Its_Sum(string dieString, int expected)
+    {
+        var die = ParseDie(dieString);
+        var actual = YetzeeGame.Score(die, Category.Pairs);
+        Assert.Equal(expected, actual);
+    }
+    
+    [Theory]
+    [InlineData("2,2,2,3,4")]
+    [InlineData("3,2,4,3,3")]
+    [InlineData("3,2,3,3,3")]
+    [InlineData("3,3,3,3,3")]
+    public void More_Than_2_Are_Not_Pairs(string dieString)
+    {
+        var die = ParseDie(dieString);
+        var actual = YetzeeGame.Score(die, Category.Pairs);
+        Assert.Equal(0, actual);   
+    }
+    
+    [Theory]
+    [InlineData("2,3,2,3,4", 6)]
+    [InlineData("3,6,6,1,3", 12)]
+    public void Pairs_Scores_The_Highest_Pair(string dieString, int expected)
+    {
+        var die = ParseDie(dieString);
+        var actual = YetzeeGame.Score(die, Category.Pairs);
+        Assert.Equal(expected, actual);   
+    }
+
+    [Theory]
+    [InlineData("1,1,2,3,3", 8)]
+    [InlineData("1,2,2,3,3", 10)]
+    public void Two_Pairs_Scores_The_Sum_Of_The_Die_In_The_Pairs(string dieString, int expected)
+    {
+        var die = ParseDie(dieString);
+        var actual = YetzeeGame.Score(die, Category.TwoPairs);
+        Assert.Equal(expected, actual);   
+    }
+    
+}
---
+++ Yetzee/Yetzee/YetzeeGame.cs
@@ -0,0 +1,63 @@
+﻿namespace Yetzee;
+
+public static class YetzeeGame
+{
+    
+    private static int Count(this IEnumerable<int> numbers, int num) =>
+        numbers.Count(it => it == num);
+
+    private static bool HasPairOf(this int[] die, int pairNumber)
+    {
+        return die.Count(pairNumber) == 2;
+    }
+
+    private static int[] FindPairs()
+    {
+        
+    }
+    
+    
+    public static int Score(int [] die, Category category)
+    {
+        if(category==Category.Yahtzee)
+            if (die.Distinct().Count() == 1)
+                return 50;
+            else
+            {
+                return 0;
+            }
+
+        if (category == Category.Ones)
+            return die.Count(1);
+        if (category == Category.Twos)
+            return die.Count(2)*2;
+        if (category == Category.Threes)
+            return die.Count(3)*3;
+        if (category == Category.Fours)
+            return die.Count(4)*4;
+        if (category == Category.Fives)
+            return die.Count(5)*5;
+        if (category == Category.Sixes)
+            return die.Count(6)*6;
+        if (category == Category.Pairs)
+        {
+            for (int i = 6; i >=1; i--)
+            {
+                if (die.HasPairOf(i))
+                    return i*2;
+            }
+
+            return 0;
+
+        }
+            
+        
+        return die.Sum();
+    }
+
+    
+}
+public enum Category
+{
+    Chance,Yahtzee,Ones,Twos,Threes,Fours,Fives,Sixes,Pairs,TwoPairs
+}
